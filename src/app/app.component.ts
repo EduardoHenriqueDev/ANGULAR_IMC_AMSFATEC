6941393bf8761713a16bddd56d118ed802f35cf3
EduardoHenriqueDev <eduhenrimacedo@gmail.com>
2025-03-08 02:55:49 -0300

    Correção Imagens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';  // Para usar ngIf, ngFor e outras diretivas
-import { FormsModule } from '@angular/forms';  // Para usar ngModel
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';  // Para usar o Bootstrap
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  standalone: true,  // Define o componente como standalone
-  imports: [CommonModule, FormsModule, NgbModule],  // Declarando os módulos necessários aqui
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgbTooltipModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -15,41 +15,40 @@ export class AppComponent {
   altura: number | null = null;
   imc: number | null = null;
   mensagem: string = '';
-  imagem: string = '';  // Propriedade para armazenar o caminho da imagem
+  imagem: string = '';
 
   calcularIMC() {
     if (this.peso && this.altura) {
       const pesoNum = +this.peso;
       let alturaNum = +this.altura;
 
-      alturaNum = alturaNum / 100;  // Convertendo altura para metros
+      alturaNum = alturaNum / 100;
 
       if (pesoNum && alturaNum) {
         this.imc = pesoNum / (alturaNum * alturaNum);
 
-        // Determinando a mensagem e a imagem baseada no IMC
         if (this.imc < 18.5) {
           this.mensagem = 'Abaixo do peso';
-          this.imagem = 'src/assets/images/abaixo_peso.png';  // Caminho da imagem
+          this.imagem = 'assets/img/abaixo_peso.png';
         } else if (this.imc >= 18.5 && this.imc < 24.9) {
           this.mensagem = 'Peso normal';
-          this.imagem = 'src/assets/images/peso_normal.png';  // Caminho da imagem
+          this.imagem = 'assets/img/peso_normal.png';
         } else if (this.imc >= 25 && this.imc < 29.9) {
           this.mensagem = 'Sobrepeso';
-          this.imagem = 'src/assets/images/sobrepeso.png';  // Caminho da imagem
+          this.imagem = 'assets/img/sobrepeso.png';
         } else {
           this.mensagem = 'Obesidade';
-          this.imagem = 'src/assets/images/obesidade.png';  // Caminho da imagem
+          this.imagem = 'assets/img/obesidade.png';
         }
       } else {
         this.imc = null;
         this.mensagem = 'Por favor, insira valores válidos de peso e altura.';
-        this.imagem = '';  // Limpar imagem em caso de erro
+        this.imagem = '';
       }
     } else {
       this.imc = null;
       this.mensagem = 'Por favor, insira os valores de peso e altura.';
-      this.imagem = '';  // Limpar imagem em caso de erro
+      this.imagem = ''; 
     }
   }
 }
